5567c4665b2e863aebc4327e26131bf593a50458
Quan <quan@gmail.com>
2022-08-11 15:20:27 +0700

    done 1108

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name = "John";
+    private String lop = "C02";
+    public Student(){}
+public void setName(String name){
+    this.name=name;
+}
+public void setLop(String lop){
+    this.lop=lop;
+}
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        Student Yasuo = new Student();
+        Student Malphite = new Student();
+        Yasuo.setName("Yasuo");
+        Malphite.setName("Malphite");
+        System.out.println(Yasuo.getName());
+    }
+}
